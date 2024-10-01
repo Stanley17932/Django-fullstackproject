700e5570ba658c7deee55970cac8f8eeb83e1186
Stanley Otieno <83674953+Stanley17932@users.noreply.github.com>
2024-10-01 21:28:57 +0300

    model creation

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -9,5 +9,5 @@ class UserSerializer(serializers.ModelSerializer):
 
 
     def create(self, validated_data):
-        user = User.objects.create_iser(**validated_data)
+        user = User.objects.create_user(**validated_data)
         return user
--- backend/api/views.py
+++ backend/api/views.py
@@ -10,5 +10,5 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 # Create your views here.
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
-    serialzer_class = User
+    serializer_class = UserSerializer
     permission_classes = [AllowAny]
