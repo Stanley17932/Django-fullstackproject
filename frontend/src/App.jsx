b1394dd675f1bb919b44ddf8768841add0af0419
Stanley Otieno <83674953+Stanley17932@users.noreply.github.com>
2025-01-13 18:30:27 +0300

    add files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,18 @@
 import react from 'react'
+import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Home from "./pages/Home"
+import NotFound from "./pages/NotFound"
+import ProtectedRoute from './components/ProtectedRoute'
+
+function logout(){
+  localStorage.clear()
+  return  <Navigate to="/login"/>
+}
 
 function App() {
+
  
 
   return (
