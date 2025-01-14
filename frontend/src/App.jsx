5dde0da01e788497c77012d89880a3cae00564f5
Stanley Otieno <83674953+Stanley17932@users.noreply.github.com>
2025-01-14 09:22:48 +0300

    add the new files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,22 @@
-import react from 'react'
-import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
-import Login from "./pages/Login"
+import react from "react"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import Login from "./pages/login"
 import Register from "./pages/Register"
 import Home from "./pages/Home"
 import NotFound from "./pages/NotFound"
 import ProtectedRoute from "./components/ProtectedRoute"
 
-function Logout(){
+function Logout() {
   localStorage.clear()
-  return  <Navigate to="/login"/>
+  return <Navigate to="/login" />
 }
 
-function RegisterAndLogout(){
+function RegisterAndLogout() {
   localStorage.clear()
   return <Register />
 }
+
 function App() {
-
- 
-
   return (
     <BrowserRouter>
       <Routes>
@@ -27,14 +25,12 @@ function App() {
           element={
             <ProtectedRoute>
               <Home />
-
             </ProtectedRoute>
           }
-
         />
-        <Route path="/login" element={<Login />}/>
-        <Route path="/logout" element={<Logout />}/>
-        <Route path="/register" element={<RegisterAndLogout />}/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/register" element={<RegisterAndLogout />} />
         <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </BrowserRouter>
--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -12,12 +12,39 @@ function Form({ route, method }) {
     const name = method === "login" ? "Login" : "Register";
 
     const handleSubmit = async (e) => {
+        setLoading(true);
         e.preventDefault();
+        try {
+            const res = await api.post(route, {
+                username,
+                password,
+            });
+            if (method === "login") {
+                localStorage.setItem(ACCESS_TOKEN, res.data.access);
+                localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
+                navigate("/");
+            }
+        } catch (error) {
+            console.log(error);
+        }
+        finally {
+            setLoading(false);
+        }
+        
     }
 
 
     return <form onSubmit={handleSubmit} className="form-container">
         <h1>name</h1>
-        <input className = "form-input" type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+        <input className = "form-input" 
+        type="text" 
+        placeholder="Username" 
+        value={username} 
+        onChange={(e) => setUsername(e.target.value)} />
+        <input className = "form-input" 
+        type="password" 
+        placeholder="Password" 
+        value={password} 
+        onChange={(e) => setPassword(e.target.value)} />
     </form>
 }
